a705a3bb43ba53e2315163bc1679843d6d50ef37
Mr-9 <1421122102@qq.com>
2017-08-06 17:10:24 +0800

    2017-08-06

---
+++ js/index.js
@@ -0,0 +1,46 @@
+//30no2 2017-08-06
+var menuBtn = document.querySelector('.menu-btn');
+var btn = document.querySelector('.menu-btn-icon');
+var menu = document.querySelector('.nav-container-list');
+var menuIsCollapse = false;
+// menu.style.display = 'none';
+
+menuBtn.onclick = function(){
+    menuIsCollapse =!menuIsCollapse;
+    if(menuIsCollapse){
+        menu.style.display = 'block';
+        btn.classList.remove('btn-icon-close');
+        btn.classList.add('btn-icon-menu');
+    }else{
+        menu.style.display = 'none';
+        btn.classList.remove('btn-icon-menu');
+        btn.classList.add('btn-icon-close');
+
+    }
+}
+
+//对于不支持的matchMedia的浏览器采用监听resize的事件
+if(window.matchMedia){
+    var mql = window.matchMedia("(min-width:480px)");
+    mql.addListener(mediaChangeHandler);
+    mediaChangeHandler(mql);
+}else{
+    window.addEventListener('resize',function(){
+        var innerWidth = window.innerWidth||document.documentElement.clientWidth||document.body.clientWidth;
+        mediaChangeHandler(
+            innerWidth >= 480 ? {matches:true} : {matches:false}
+        )
+    },false);
+}
+
+function mediaChangeHandler(mql){
+    if(mql.matches){
+        menu.style.display = '';
+    }else{
+        if(menuIsCollapse){
+            menu.style.display = 'none';
+        }else{
+            menu.style.display = '';
+        }
+    }
+}
